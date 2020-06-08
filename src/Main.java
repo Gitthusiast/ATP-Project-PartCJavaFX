70bf7d8759b46585a0fb1669d51c2ba096f09458
yairch <50925578+yairch@users.noreply.github.com>
2020-06-08 21:46:35 +0300

    Started working on model

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader();
         Parent root = fxmlLoader.load(getClass().getResource("View/MyView.fxml").openStream());
+        root.getStylesheets().add(getClass().getResource("View/Covid19Style.css").toString());
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
